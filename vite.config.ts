1efa81b21163861dbcf91b0fda54d6d7d3a12bb5
jeffery8910 <jeffery8910@gmail.com>
2025-06-25 23:37:14 +0800

    Refine the Package Statement

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
       base: '/smc-strategy/',
-      plugins: [react(), tailwindcss()],
+      plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)

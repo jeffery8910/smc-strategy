8c0c6dc13995d02f10d3b3641fce92819622e00b
jeffery8910 <jeffery8910@gmail.com>
2025-06-25 22:54:24 +0800

    \"Fix: Use absolute paths in Tailwind content config\"

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,12 @@
+const path = require('path');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./App.tsx",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    path.resolve(__dirname, './index.html'),
+    path.resolve(__dirname, './App.tsx'),
+    path.resolve(__dirname, './pages/**/*.{js,ts,jsx,tsx}'),
+    path.resolve(__dirname, './components/**/*.{js,ts,jsx,tsx}'),
   ],
   theme: {
     extend: {},
